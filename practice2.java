7b5c0d0bedd214ba0014e0ebfeaf01f73edeac05
Abutalib2002 <111528358+Abutalib2002@users.noreply.github.com>
2022-08-24 11:16:10 +0530

    Add files via upload

---
+++ practice2.java
@@ -0,0 +1,34 @@
+
+public class practice2{
+
+    public static void main(String args[]){
+
+//practicing looping statements;
+//  declaring variable;
+int a;
+a = 0;//assigning varaiable with value;
+// Printing value of a and increament by whie loop;
+while(a<10){
+    System.out.print("\t" + a + "\t");
+    a++;
+}
+
+
+
+// using for loop
+int f;
+
+for(f =0 ;f<20;f++)
+{
+    System.out.println(f);
+}// using do while loop
+int e=0;
+do{
+    System.out.print("Do while loop statement"+"\t"+"\n");
+    e++;
+}while(e<10);
+
+
+
+    }
+}
