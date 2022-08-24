2f40631b56fe55e3735ea53d818a395d59f4ec19
Abutalib2002 <111528358+Abutalib2002@users.noreply.github.com>
2022-08-24 14:58:13 +0530

    Add files via upload

---
+++ practice4.java
@@ -0,0 +1,25 @@
+
+public class practice4{
+
+    public static void main(String args[]){
+
+
+
+
+
+
+
+int a=10,b=19;
+float c=80.963F,d=9.632F;
+System.out.println("Increament of a:"+ a++ +"\t" + a);
+System.out.println("Increament of b:"+ ++b + "\t" + b);
+System.out.println("Increament of c:"+ c-- + "\t" + c);
+System.out.println("Increament of d:"+ --d + "\t" + d);
+ 
+
+    }
+}
+
+
+
+
