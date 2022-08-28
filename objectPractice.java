74b2ed5fd99a10b409bdca891e2b6eceb83a2f19
Abutalib2002 <111528358+Abutalib2002@users.noreply.github.com>
2022-08-28 22:46:26 +0530

    Add files via upload

---
+++ objectPractice.java
@@ -0,0 +1,27 @@
+
+public class objectPractice{
+
+    public static void main(String args[]){
+
+
+First prints = new First();
+
+prints.welcome();
+prints.hello();
+prints.world();
+    }
+}
+class First{
+    public void welcome(){
+        System.out.println("hi");
+    }
+    
+    
+    public void hello(){
+        System.out.println("hello");
+    }
+    
+    public void world(){
+        System.out.println("world");
+    }
+}
