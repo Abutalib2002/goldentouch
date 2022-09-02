87e9860bac2706a4d8b09a171741686064bc09a9
Abutalib2002 <111528358+Abutalib2002@users.noreply.github.com>
2022-09-03 01:02:44 +0530

    Add files via upload

---
+++ PRctice1.java
@@ -0,0 +1,80 @@
+package oops;
+
+public class PRctice1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+printName dbms = new printName();
+
+dbms.sugan();
+System.out.println("\n");
+dbms.arun();
+System.out.println("\n");
+dbms.rahim();
+System.out.println("\n");
+dbms.farhan();
+System.out.println("\n");
+dbms.kiran();
+System.out.println("\n");
+dbms.madhan();
+	}
+
+}
+class printName{
+	
+	public void sugan(){
+		System.out.println("NAME:sugan");
+		System.out.println("AGE:19");
+		System.out.println("PROFESSION:Student");
+		System.out.println("CLASS:12th");
+		System.out.println("ID:012546");
+		System.out.println("RESULT:PASS");
+
+	}
+	
+	public void arun() {
+		System.out.println("NAME:arun");
+		System.out.println("AGE:19");
+		System.out.println("PROFESSION:Student");
+		System.out.println("CLASS:12th");
+		System.out.println("ID:012547");
+		System.out.println("RESULT:PASS");
+
+	}
+	public void rahim() {
+		System.out.println("NAME:Rahim");
+		System.out.println("AGE:19");
+		System.out.println("PROFESSION:Student");
+		System.out.println("CLASS:12th");
+		System.out.println("ID:012548");
+		System.out.println("RESULT:PASS");
+
+	}
+	public void farhan() {
+		System.out.println("NAME:Farhan");
+		System.out.println("AGE:18");
+		System.out.println("PROFESSION:Student");
+		System.out.println("CLASS:12th");
+		System.out.println("ID:012549");
+		System.out.println("RESULT:PASS");
+
+	}
+	public void kiran() {
+		System.out.println("NAME:Kiran");
+		System.out.println("AGE:20");
+		System.out.println("PROFESSION:Student");
+		System.out.println("CLASS:12th");
+		System.out.println("ID:012550");
+		System.out.println("RESULT:FAIL");
+
+	}
+	public void madhan() {
+		System.out.println("NAME:Madhan");
+		System.out.println("AGE:17");
+		System.out.println("PROFESSION:Student");
+		System.out.println("CLASS:12th");
+		System.out.println("ID:012551");
+		System.out.println("RESULT:PASS");
+
+	}
+}
