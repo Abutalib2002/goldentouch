7dea3b57c3e4ce3b87dbeebdb0aa387358e29f3d
Abutalib2002 <111528358+Abutalib2002@users.noreply.github.com>
2022-09-22 00:06:47 +0530

    Add files via upload

---
+++ Pattern1.java
@@ -0,0 +1,15 @@
+package JavaSession;
+
+public class Pattern1 {
+public static void main(String[] args) {
+	System.out.println("----------           ----          ---        ---           ---     ");
+	System.out.println("    ||              //  \\\\          \\\\        ///           // \\\\    ");
+	System.out.println("    ||             //    \\\\          \\\\      ///           //   \\\\  ");
+	System.out.println("    ||            //      \\\\          \\\\    ///           //     \\\\");
+	System.out.println("    ||           //========\\\\          \\\\  ///           //=======\\\\");
+	System.out.println("    ||          //          \\\\          \\\\///           //         \\\\");
+	System.out.println("--  ||         //            \\\\         |__|           //           \\\\");
+	System.out.println("||__||                                                               ");
+	System.out.println("|____|");
+}
+}
