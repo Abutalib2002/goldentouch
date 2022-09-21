13b2c99dcb47d0de0793e2dd09cded21581eb7a3
Abutalib2002 <111528358+Abutalib2002@users.noreply.github.com>
2022-09-22 01:02:58 +0530

    Add files via upload

---
+++ SumOfNUmbers2.java
@@ -0,0 +1,19 @@
+package JavaSession;
+
+public class SumOfNUmbers2 {
+public static void main(String[] args) {
+	int num1;
+	int num2;
+	int num3 = 77;
+	num1 = 23;
+	num2 = 29;
+	int sum;
+	sum = num1 + num2 +num3;
+	int Product = num1*num2*num3;
+	System.out.println("The given number num1 is:  " +num1);
+	System.out.println("The given number num1 is:  " +num2);
+	System.out.println("The given number num1 is:  " +num3);
+	System.out.println("The given number num1 and num2 and num3  of sum  is:  " +sum);
+	System.out.println("The given number num1 and num2 and num3 of Product  is:  " +Product);
+}
+}
