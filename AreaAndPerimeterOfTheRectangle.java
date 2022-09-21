0cc580de81379d04dbddbd48b62f70fcdd8a4153
Abutalib2002 <111528358+Abutalib2002@users.noreply.github.com>
2022-09-22 00:05:44 +0530

    Add files via upload

---
+++ AreaAndPerimeterOfTheRectangle.java
@@ -0,0 +1,15 @@
+package JavaSession;
+
+public class AreaAndPerimeterOfTheRectangle {
+public static void main(String[] args) {
+	double Length = 1.2;
+	double Width = 3.6;
+	double Area;
+	double Perimeter;
+	Area = Length*Width;
+	Perimeter = 2*Length+2*Width;
+	
+	System.out.println("Area of the Rectangle:  " + Area);
+	System.out.println("Perimeter of the Rectangle:  " + Perimeter);
+}
+}
