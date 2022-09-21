4c35bdd629fe5998d0449062abb718281d6fa5e7
Abutalib2002 <111528358+Abutalib2002@users.noreply.github.com>
2022-09-21 13:45:11 +0530

    Add files via upload

---
+++ AreaRadiusCircumfrenceofCircle.java
@@ -0,0 +1,17 @@
+package JavaSession;
+
+public class AreaRadiusCircumfrenceofCircle {
+public static void main(String[] args) {
+	double radius,area,circumfrence;
+	
+	final double PI = 3.14159265;
+	radius = 1.2;
+	area = radius*radius*PI;
+	circumfrence = 2.0*radius*PI;
+	System.out.println("Radius of the circle: " + radius);
+	System.out.println("Area of the circle is: "  + area);
+	System.out.println("CircumFernce of the circle: "+circumfrence);
+}
+
+
+}
