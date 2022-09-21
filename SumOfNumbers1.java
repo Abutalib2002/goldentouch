1ebe9d1e3e7da2280c4a0d516cfb510d098bffd4
Abutalib2002 <111528358+Abutalib2002@users.noreply.github.com>
2022-09-22 01:04:02 +0530

    Add files via upload

---
+++ SumOfNumbers1.java
@@ -0,0 +1,14 @@
+package JavaSession;
+
+public class SumOfNumbers1 {
+public static void main(String[] args) {
+	int a = 25;
+	int b = 53;
+	int Add;
+	Add = 31*a+17*b;
+	System.out.println("The given number a is: " + a);
+	System.out.println("The given number b is: " + b);
+	System.out.println("The given number a and b is  multiple by 31 timmes of a and 17 times of b is:  " + Add);
+	
+}
+}
