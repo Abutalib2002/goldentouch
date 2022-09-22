67f128ab0f23e6a81f052d46691b11ceea941b82
Abutalib2002 <111528358+Abutalib2002@users.noreply.github.com>
2022-09-22 09:21:42 +0530

    Add files via upload

---
+++ CheckOddOREven.java
@@ -0,0 +1,13 @@
+package JavaSession;
+
+public class CheckOddOREven {
+public static void main(String[] args) {
+	int number =1;
+	if(number%2==0) {
+		System.out.println("This number is Even");
+	}
+	else {
+		System.out.println("This number is odd");
+	}
+}
+}
