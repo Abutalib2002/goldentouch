21f6e1ea428e4efa7fb52849402ea2cde29793d8
Abutalib2002 <111528358+Abutalib2002@users.noreply.github.com>
2022-10-09 02:46:31 +0530

    Add files via upload

---
+++ Addint.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+
+
+int number1,number2,addValue;
+printf("Enter the value to ADD:\n");
+scanf("%d%d",&number1,&number2);
+addValue = number1 + number2;
+printf("The addition value is: \t%d",addValue);
+return 0;
+
+}
